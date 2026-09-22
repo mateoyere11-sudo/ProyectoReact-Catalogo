9ee83345f3e4ea912aff9f73f3b9d0d7ca31af70
mateoyere11-sudo <mateoyere11@gmail.com>
2026-09-22 07:31:02 -0500

    Cambio en CarritoCompras

--- src/pages/Compra/CarritoCompras.jsx
+++ src/pages/Compra/CarritoCompras.jsx
@@ -119,9 +119,6 @@ function CarritoCompras(){
                 <h3 className="font-semibold text-slate-800 dark:text-slate-100 truncate">
                   {item.name}
                 </h3>
-                <p className="text-sm text-slate-500 dark:text-slate-400">
-                  {item.episodio}
-                </p>
                 <p className="text-sm text-slate-500 dark:text-slate-400 mt-1">
                   Precio unitario: {formatearPrecio(item.precio)}
                 </p>
